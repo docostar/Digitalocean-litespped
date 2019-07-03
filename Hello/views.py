fec0bdb601ca7a49229f55a45d538668da09999c
test-git <dholariyarahul@gmail.com>
2019-07-03 12:16:09 +0530

    H1 tag addead

--- Hello/views.py
+++ Hello/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, Docobhai , Saru ne., Maja mane")
+    return HttpResponse("Hello, Docobhai , Saru ne., Maja mane <h1> Digital Ocean Litespeed sucessfull</h1>")
 
 # Create your views here.
